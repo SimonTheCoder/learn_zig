4452286a7b2792278ff744c55f020f30ea290e0a
SimonTheCoder <shihangyu@gmail.com>
2025-12-16 21:46:15 +0800

    tried some example from offical site

---
+++ 20-reflection.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+const Header = struct {
+    magic: u32,
+    name: []const u8,
+};
+
+pub fn main() void {
+    printInfoAboutStruct(Header);
+}
+
+fn printInfoAboutStruct(comptime T: type) void {
+    const info = @typeInfo(T);
+    inline for (info.@"struct".fields) |field| {
+        std.debug.print(
+            "{s} has a field called {s} with type {s}\n",
+            .{
+                @typeName(T),
+                field.name,
+                @typeName(field.type),
+            },
+        );
+    }
+}
+
---
+++ test_comptime_variables.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "comptime vars" {
+    var x: i32 = 1;
+    comptime var y: i32 = 1;
+
+    x += 1;
+    y += 1;
+
+    try expect(x == 2);
+    try expect(y == 2);
+    // @compileLog("y value", y);
+
+    std.log.warn("\ny value {d}\n", .{y});
+
+    if (y != 2) {
+        // This compile error never triggers because y is a comptime variable,
+        // and so `y != 2` is a comptime value, and this if is statically evaluated.
+        @compileLog("wrong y value", y);
+    }
+}
